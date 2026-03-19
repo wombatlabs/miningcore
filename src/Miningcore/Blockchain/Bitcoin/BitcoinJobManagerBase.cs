dd887331c23f7a4db3bfd80d2a339f31d3ff6751
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2026-03-19 15:45:09 -0700

    Reformat cash address

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
@@ -576,15 +576,10 @@ public abstract class BitcoinJobManagerBase<TJob> : JobManagerBase<TJob>
                 return BitcoinUtils.BechSegwitAddressToDestination(poolConfig.Address, network, extraPoolConfig?.BechPrefix);
 
             case BitcoinAddressType.BCash:
-                try
-                {
-                    return BitcoinUtils.CashAddrToDestination(poolConfig.Address, extraPoolConfig?.CashAddrPrefix);
-                }
-                catch(FormatException)
-                {
-                    // fall back to legacy BCH parsing for backwards compatibility
-                    return BitcoinUtils.BCashAddressToDestination(poolConfig.Address, network);
-                }
+                return BitcoinUtils.BCashAddressToDestination(poolConfig.Address, network);
+
+            case BitcoinAddressType.CashAddr:
+                return BitcoinUtils.CashAddrToDestination(poolConfig.Address, extraPoolConfig?.CashAddrPrefix);
 
             case BitcoinAddressType.Litecoin:
                 return BitcoinUtils.LitecoinAddressToDestination(poolConfig.Address, network);
